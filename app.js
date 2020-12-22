82d7b888a82e5018e26aec11ce04eb2a43a88f1b
Dat Pham <datmpham96@gmail.com>
2020-12-22 10:37:52 -0600

    basic html and javascript structure

---
+++ app.js
@@ -0,0 +1,14 @@
+// step 1 take input from user
+function watchSubmit(){
+    console.log('inside watchSubmit');
+}
+// step 2 using input from user to make api call
+function getDataFromApi(){
+    console.log('inside getDataFromApi');
+}
+// step 3 using api result display then to the user
+function displaySearchResult(){
+    console.log('inside displaySearchResult');
+}
+
+$(watchSubmit);
