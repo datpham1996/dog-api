9efd64da96ec708faa356d4e5ebee7862aa41049
Dat Pham <datmpham96@gmail.com>
2020-12-31 14:00:54 -0600

    html and js changes

--- app.js
+++ app.js
@@ -1,29 +1,26 @@
 "use strict";
-//Pass numInput, which represents an integer as an argument
-function getDogImage(numInput) {
-  if (numInput < 3) {
-    fetch("https://dog.ceo/api/breeds/image/random/3")
+function getDogImages(query, displayCallback) {
+  if (query < 3) {
+    fetch(`https://dog.ceo/api/breeds/image/random/${query}`)
       .then(response => response.json())
       .then(responseJson => console.log(responseJson));
-  } else if (numInput > 50) {
-    return alert("Please choose a number equal or less than 50");
+  } else if (query > 50) {
+    return alert(`Please choose a number equal or less than 50`);
   } else {
-    fetch(`https://dog.ceo/api/breeds/image/random/${numInput}`)
+    fetch(`https://dog.ceo/api/breeds/image/random/${query}`)
       .then(response => response.json())
       .then(responseJson => console.log(responseJson));
   }
 }
 
-function watchUserInput() {
-  $("#dog-num-form").submit(e => {
-    e.preventDefault();
-    let userNumInput = $("#num-dog").val();
-    //Pass the number value to getDogImage
-    getDogImage(userNumInput);
+function listenToInput() {
+  $('.js-search-form').submit(event => {
+    event.preventDefault();
+    const queryTarget = $(event.currentTarget).find('.js-query');
+    getDogImages('.js-query');
   });
 }
-
-$(function() {
-  console.log("App loaded! Waiting for submit!");
-  watchUserInput();
-});
+$(function () {
+  console.log('App loaded! Waiting for submit!');
+  listenToInput();
+}); 
